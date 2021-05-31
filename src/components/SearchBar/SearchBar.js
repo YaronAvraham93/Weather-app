eea94fbe9d087ac36725bb16c2409cdc1db1534d
Yaron <yaronavraham600@gmail.com>
2021-05-31 15:56:43 +0300

    change css for input

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,36 +1,29 @@
 import React from 'react';
 import styled from 'styled-components';
 
-
 const Inpout = styled.input`
-  display: block;
-  width: 250px;
-  height: 25px;
-  padding-left: 1rem;
-  padding-right: 1rem;
+  opacity: 0.9;
+  font-size: 20px;
   border-radius: 0.85rem;
-  border: 0.3px solid;
-  border-color: inherit;
-  background-color: #fff;
+  border: none;
 
   &::focus {
     border-color: transparent;
   }
- 
 `;
 const Form = styled.form`
   display: flex;
   justify-content: center;
   margin-top: 35px;
-  @media  (max-width: 768px) {
+  @media (max-width: 768px) {
     margin: 0;
-}
-&:focus-within ul{
-    display:block;
+  }
+  &:focus-within ul {
+    display: block;
   }
 `;
-const DropDwonListLi=styled.li`
-list-style:none;
+const DropDwonListLi = styled.li`
+    list-style:none;
     padding: 5px 26px;
     background-color: white;
     border-radius: 5px;
@@ -42,43 +35,35 @@ cursor:pointer;
 background-color:whitesmoke;
 
 }`;
-const DropDwonListUl=styled.ul`
-display:none;
-width: 23%;
-border-radius:10px;
-position:absolute;
-background-color:whitesmoke;
-overflow:hidden;
-margin-top:29px;
-padding:0;
+const DropDwonListUl = styled.ul`
+  display: none;
+  width: 18%;
+  border-radius: 10px;
+  position: absolute;
+  background-color: whitesmoke;
+  overflow: hidden;
+  margin-top: 29px;
+  padding: 0;
+  @media (max-width: 768px) {
+    width: 48%;
+  }
 `;
 
-export default function SearchBar({autoComplete,placeholder,value,onChange,selectFromList}) {
-  
-  const list=autoComplete
-  ? autoComplete.map((v,i) => {
-    return(
-      <DropDwonListLi onMouseDown={()=>selectFromList(v)} key={i}>
-        {v.LocalizedName}
-      </DropDwonListLi>
-    )
-
-  }) : [];
+export default function SearchBar({ autoComplete, placeholder, value, onChange, selectFromList }) {
+  const list = autoComplete
+    ? autoComplete.map((v, i) => {
+        return (
+          <DropDwonListLi onMouseDown={() => selectFromList(v)} key={i}>
+            {v.LocalizedName}
+          </DropDwonListLi>
+        );
+      })
+    : [];
   return (
-    
     <Form>
-       
-      <Inpout 
-      placeholder={placeholder} 
-     value={value} 
-      onChange={onChange} 
-      />
-      
-     <DropDwonListUl> {list} </DropDwonListUl>
-    
+      <Inpout placeholder={placeholder} value={value} onChange={onChange} />
+
+      <DropDwonListUl> {list} </DropDwonListUl>
     </Form>
-
-           
-
   );
 }
